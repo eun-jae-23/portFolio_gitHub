959d1114395020e352bfd4a232bad087b201c5ef
eun-jae-23 <listening232@gmail.com>
2025-06-16 20:25:29 +0900

    0616//20:25

--- contents/js/brand_bdstory.js
+++ contents/js/brand_bdstory.js
@@ -1,6 +1,29 @@
 $(function () {
     $('header').addClass('header-scrolled');
 
+    //oneShow(), otherShow()를 위한 사전 세팅 시작 ////////////////////////////
+    //1) .salady-story의 제목, article에 해당하는 모든 자식요소 투명도 0
+    $('.salady-story').children().css({
+        opacity: 0
+    });
+
+    //2) .title-wrap 좌측 자식요소 마진left -200, 우측 자식요소 마진right -200
+    $('.salady-story .title-wrap .title').css({
+        marginLeft: -300
+    })
+    $('.salady-story .title-wrap .des').css({
+        marginRight: -300
+    })
+
+    //3) article 좌측 자식요소 img 마진left -200, 우측 자식요소 .content 마진right -200
+    $('.salady-story article img').css({
+        marginLeft: -300
+    })
+    $('.salady-story article .content').css({
+        marginRight: -300
+    })
+
+    //사전 세팅 시작 끝 ////////////////////////////
 
     oneScrollEvt();
 });
@@ -17,10 +40,150 @@ function oneScrollEvt() {
         if (!isExecuted) {
             $('html, body').animate({
                 scrollTop: finalScrollTop
-            }, 800);
+            }, 800, oneShow);
             isExecuted = true;
+            $(window).off('scroll');
         }
     });
 }
 
+function oneShow() {
 
+    //oneScrollEvt 끝나고 콜백함수로 처음 보여져야 할 요소들
+    //.titlewrap
+    //좌측 요소
+    $('.salady-story .title-wrap').animate({
+        opacity: 1
+    }, 500)
+        .find('.title').animate({
+            marginLeft: 0
+        }, 500)
+    //우측 요소
+    $('.salady-story .title-wrap').animate({
+        opacity: 1
+    })
+        .find('.des').animate({
+            marginRight: 0
+        }, 500);
+
+    //start
+    //좌측 요소
+    $('.salady-story .start').delay(500).animate({
+        opacity: 1
+    }, 500)
+        .find('img').delay(500).animate({
+            marginLeft: 0
+        }, 500)
+        //우측 요소
+        .parents('.start')
+        .find('.content').delay(500).animate({
+            marginRight: 0
+        }, 500);
+}
+
+
+
+function changeScrollEvt() {
+    let isExecuted = false;
+    let changeScrollTop = $('.salady-story .change').scrollTop();
+    let finalScrollTop = changeScrollTop - $('header').height();
+
+    $(window).on('scroll', function () {
+        if (!isExecuted) {
+            $('html, body').animate({
+                scrollTop: finalScrollTop
+            }, 800, changeShow);
+            isExecuted = true;
+            $(window).off('scroll');
+        }
+    });
+}
+
+function changeShow() {
+
+    //changeScrollEvt 끝나고 콜백함수로 처음 보여져야 할 요소들
+    //.change
+    //좌측 요소
+    $('.salady-story .change').animate({
+        opacity: 1
+    }, 500)
+        .find('img').animate({
+            marginLeft: 0
+        }, 500).parents('.change')
+        //우측 요소
+        .find('.content').animate({
+            marginRight: 0
+        }, 500);
+
+}
+
+
+
+function growScrollEvt() {
+    let isExecuted = false;
+    let growScrollTop = $('.salady-story .grow').scrollTop();
+    let finalScrollTop = growScrollTop - $('header').height();
+
+    $(window).on('scroll', function () {
+        if (!isExecuted) {
+            $('html, body').animate({
+                scrollTop: finalScrollTop
+            }, 800, growShow);
+            isExecuted = true;
+            $(window).off('scroll');
+        }
+    });
+}
+
+function growShow() {
+
+    //growScrollEvt 끝나고 콜백함수로 처음 보여져야 할 요소들
+    //.grow
+    //좌측 요소
+    $('.salady-story .grow').animate({
+        opacity: 1
+    }, 500)
+        .find('img').animate({
+            marginLeft: 0
+        }, 500).parents('.grow')
+        //우측 요소
+        .find('.content').animate({
+            marginRight: 0
+        }, 500);
+
+}
+
+function futureScrollEvt() {
+    let isExecuted = false;
+    let futureScrollTop = $('.salady-story .future').scrollTop();
+    let finalScrollTop = futureScrollTop - $('header').height();
+
+    $(window).on('scroll', function () {
+        if (!isExecuted) {
+            $('html, body').animate({
+                scrollTop: finalScrollTop
+            }, 800, futureShow);
+            isExecuted = true;
+            $(window).off('scroll');
+        }
+    });
+}
+
+function futureShow() {
+
+    //futureScrollEvt 끝나고 콜백함수로 처음 보여져야 할 요소들
+    //.future
+    //좌측요소
+    $('.salady-story .future').animate({
+        opacity: 1
+    }, 500)
+        .find('.content').animate({
+            marginRight: 0
+        }, 500).parents('.future')
+        //우측 요소    
+        .find('img').animate({
+            marginLeft: 0
+        }, 500);
+
+
+}
--- contents/js/faq_common.js
+++ contents/js/faq_common.js
@@ -2,34 +2,53 @@
 
 //호출
 $(function () {
-    //아코디언 초기 세팅:첫번째 .faq-box의 .a-box 제외 다 hide처리
-    $('.faq-box').find('.a-box').hide();
-    $('.faq-box:first').find('.a-box').show();
+    //아코디언 초기 세팅
+    //변수값 할당
+    $faqBox = $('.faq-box');
+    $aBox = $('.a-box');
 
+    //1) 모든 .faq-box 하위요소 .a-box display none처리
+    $faqBox.find($aBox).css({
+        display: 'none'
+    });
+
+    //2) .on클래스 하위 요소 .a-box만 display flex처리
+    $('.faq-wrap .on').find($aBox).css({
+        display: 'flex'
+    });
+
+    //3) .on클래스 하위요소 .btn-wrap img 변경
+    $('.faq-wrap .on').find('.btn-wrap img').attr('src',"../common-img/common_icon_chevron-3_top.png");
+
+    //아코디언 효과
     accordionBox();
-
 });
 
 
 ///////////////////////// 변수와 함수 ////////////////////////////////
 
+//변수 선언
+let $faqBox;
+let $aBox;
+
+//함수 accordionBox();
 function accordionBox() {
 
     //faq-box 제일 큰 부모
 
     //이벤트 발생 : .faq-box를 클릭하면
-    $('.faq-box').on('click', function () {
+    $faqBox.on('click', function () {
 
 
         //1) this에 on클래스 추가 ,하위 선택자 중 .a-box찾아서 슬라이드다운,
-        $(this).addClass('on').find('.a-box').slideDown()
+        $(this).addClass('on').find($aBox).stop(true,true).slideDown()
         //2) this의 siblings에 on클래스 제거, 그 자식들의 하위 선택자 중 모든.a-box를 슬라이드업
-        .parents('.faq-box').siblings().removeClass('on').find('.a-box').slideUp();
+        .parents($faqBox).siblings().removeClass('on').find($aBox).slideUp();
 
 
         //3) .accordion-arrow img (this) attr로 src "../common-img/common_icon_chevron-3_btm.png"로 변경
-        $(this).find('.btn-wrap img').attr('src',"../common-img/common_icon_chevron-3_btm.png");
-        $(this).siblings().find('.btn-wrap img').attr('src',"../common-img/common_icon_chevron-3_top.png");
+        $(this).find('.btn-wrap img').attr('src',"../common-img/common_icon_chevron-3_top.png");
+        $(this).siblings().find('.btn-wrap img').attr('src',"../common-img/common_icon_chevron-3_btm.png");
 
     });
 
